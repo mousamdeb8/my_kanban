31db4f0db21f96798921b2df0898757099785761
mousamdeb8 <er.mousam2@gmail.com>
2026-03-20 09:30:51 +0530

    removed dark

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import './force-light-mode.css';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
